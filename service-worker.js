5f32e4fee2157aa603b0118860690f6af37efc48
CrisAmarante <amarante_s@hotmail.com>
2025-09-26 06:51:12 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,32 +5,51 @@ const urlsToCache = [
     '/main.js',
     '/style.css',
     '/manifest.json',
-    // Adicione os caminhos dos seus ícones aqui
-    '/icon-192x192.png', 
+    '/icon-192x192.png',
     '/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna o recurso do cache se estiver disponível
-        if (response) {
-          return response;
-        }
-        // Se não estiver no cache, faz a requisição normal
-        return fetch(event.request);
-      }
-    )
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Retorna do cache se disponível
+                if (response) {
+                    return response;
+                }
+                // Requisição normal se não estiver em cache
+                return fetch(event.request)
+                    .then(networkResponse => {
+                        // Cacheia a nova resposta
+                        if (event.request.method === 'GET') {
+                            const responseClone = networkResponse.clone();
+                            caches.open(CACHE_NAME).then(cache => {
+                                cache.put(event.request, responseClone);
+                            });
+                        }
+                        return networkResponse;
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
 });
