fd9d560a0e2b32a03c70306819bd22b5af0996c5
CrisAmarante <amarante_s@hotmail.com>
2025-09-26 06:55:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // ====================================================================
 
 // ATENÇÃO: SUBSTITUA PELA URL ÚNICA DO SEU APPS SCRIPT
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwY09U1g4o2U8343_9m_ahBaAs9k2QDO1ZTeTx16trmY5oCPRXKx1XwiGiw4EvG0Qc/exec'; 
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyThRicyiLhf8_pI8gpiM7bv0GvAQYX-KZQwhU7DeHEif9hVDBMo3tSfHrT66UHTimP/exec'; 
 
 // Variável global para rastrear o ID do acidente ativo na sessão
 let currentAccidentID = null;
